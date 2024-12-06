8514c0332f19272b945a1feeea0c051037758088
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-12-06 18:45:10 -0300

    Adiciona Portfolio no Django Admin com configuração básica

--- meu_portfolio/portfolio/admin.py
+++ meu_portfolio/portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Portfolio
 
-# Register your models here.
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')  # Mostra título e data de criação na lista de objetos
+    search_fields = ('title',)  # Adiciona uma barra de busca para títulos
+    list_filter = ('created_at',)  # Filtro lateral por data de criação
+
