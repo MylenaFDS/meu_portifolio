b546244295f1e8f2591d986bf99d211077b102fe
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-12-06 18:18:51 -0300

    Add PostList and PostDetail views

--- meu_portfolio/portfolio/views.py
+++ meu_portfolio/portfolio/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Portfolio  # ou Post, dependendo do modelo
 
-# Create your views here.
+# View para listar todos os posts
+def post_list(request):
+    posts = Portfolio.objects.all()  # Substitua Portfolio por Post, se for esse o seu modelo
+    return render(request, 'portfolio/post_list.html', {'posts': posts})
+
+# View para detalhes de um post
+def post_detail(request, pk):
+    post = get_object_or_404(Portfolio, pk=pk)  # Substitua Portfolio por Post, se for esse o seu modelo
+    return render(request, 'portfolio/post_detail.html', {'post': post})
